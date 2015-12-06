09c42be89484ea915dd3d1d5ca08377d1a362e31
jawherbejaoui92 <jawherbejaoui.92@gmail.com>
2015-12-06 15:40:35 +0100

    jawher final

--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/Fragment_publier.java
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/Fragment_publier.java
@@ -1,17 +1,21 @@
 package com.androidbelieve.drawerwithswipetabs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Jawher B on 08/11/2015.
  */
 public class Fragment_publier extends Fragment {
+
+
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.connexion,null);
@@ -22,7 +26,19 @@ public class Fragment_publier extends Fragment {
         }
         View rootView = inflater.inflate(R.layout.fragment_publier, container, false);
 
+        Button publier =(Button)rootView.findViewById(R.id.buttonpublier);
+        publier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getActivity(), Map_depart.class);
+                getActivity().startActivity(myIntent);
+            }
+        });
+
+
 
         return rootView;
     }
- }
+
+
+}
